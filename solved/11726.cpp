f1241bbd03b7156726a98962736d0e8c7f4bfc57
jnghwn <jxxnghwxn@gmail.com>
2024-07-07 20:41:33 +0900

    Initial commit

---
+++ solved/11726.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+
+int n;
+
+vector<int> fi;
+
+int main() {
+    cin >> n;
+    fi.resize(n + 1, -1);
+
+    fi[1] = 1;
+    fi[2] = 2;
+
+    if (n > 2) {
+        for (int i = 3; i < n + 1; ++i) {
+            fi[i] = (fi[i - 1] + fi[i - 2]) % 10007;
+        }
+    }
+    cout << fi[n];
+
+    return 0;
+}
---
+++ solved/1260.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <utility>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::queue;
+using std::stack;
+using std::pair;
+
+int n, m, v;
+
+const int MAX = 999;
+
+
+vector<vector<int>> adj;
+
+
+stack<int, int> s;
+
+
+void INPUT() {
+    cin >> n >> m >> v;
+
+
+    adj.resize(m, vector(2, 0));
+    for (int i = 0; i < m; ++i) {
+        cin >> adj[i][0] >> adj[i][1];
+    }
+}
+
+void test() {
+    for (int i = 0; i < m; ++i) {
+        cout << adj[i][0] << " " << adj[i][1] << endl;;
+    }
+}
+
+void DPS() {
+}
+
+void BFS() {
+    vector<bool> visited;
+    visited.resize(n, false);
+    queue<int> q;
+
+    visited[v] = true;
+    q.push(v);
+
+    while (!q.empty()) {
+        int j = q.front();
+        cout << j << " ";
+        q.pop();
+
+        for (int k : adj[j]) {
+            if (!visited[k]) {
+                visited[k] = true;
+                q.push(k);
+            }
+        }
+    }
+}
+
+int main() {
+    INPUT();
+    test();
+
+    return 0;
+}
