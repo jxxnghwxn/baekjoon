4e01868df0730826a7d1989a17cad982d842f5a7
jnghwn <jxxnghwxn@gmail.com>
2024-07-27 18:08:15 +0900

    baekjoon

---
+++ solved/1904.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 15746;
+int n;
+
+vector<int> dp(0);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    dp.resize(n);
+
+    //solve
+    // 1 or 00
+    dp[0] = 1;
+    dp[1] = 2;
+    for (int i = 2; i < n; ++i) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+    }
+
+    cout << dp[n - 1] << endl;
+    return 0;
+}
